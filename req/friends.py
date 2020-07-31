a1e20fedc5bedd2dc28eb97c335478f43c05b260
gorskii <egor.yakayaka@gmail.com>
2020-07-31 05:28:47 +0300

    Add all necessary methods. Add test cases for entire friends.py module. Improve logging and docstrings.

--- req/friends.py
+++ req/friends.py
@@ -1,6 +1,9 @@
 """VK API client that calculates age distribution of user's friends"""
 import logging
-from typing import List, Tuple, Optional
+import re
+from collections import Counter
+from datetime import date
+from typing import List, Tuple, Optional, Dict, Any
 
 import requests
 
@@ -8,14 +11,15 @@ import requests
 ACCESS_TOKEN = 'cf5c063dcf5c063dcf5c063dcacf2f3021ccf5ccf5c063d9042035ee1dacc55caec56d9'
 
 # Configure logging
-friends_logger = logging.getLogger(__name__)
-friends_logger.setLevel("DEBUG")
+friends_logger = logging.getLogger('friends.py')
+friends_logger.setLevel('DEBUG')
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel("WARNING")
-
-file_handler = logging.FileHandler(filename='debug.log')
-file_handler.setLevel("DEBUG")
+console_handler.setLevel('INFO')
+# Write new log on each run
+file_handler = logging.FileHandler(filename='debug.log',
+                                   mode='w', encoding='utf-8')
+file_handler.setLevel('DEBUG')
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - '
                               '%(levelname)s - %(message)s')
@@ -28,14 +32,19 @@ friends_logger.addHandler(file_handler)
 
 
 def get_user_id_by_username_or_id(uid: str) -> Optional[int]:
-    """Return user_id
+    """Return user_id.
 
-    Get user_id by username or id
+    Gets user_id by username or id.
+    Returns None if no user was found for specified username or id.
 
-    Method docs: https://vk.com/dev/users.get
+    API Method docs: https://vk.com/dev/users.get
 
     VK API version: 5.71
     """
+    if not uid:
+        friends_logger.warning('No username or id provided')
+        return None
+
     request = requests.get(f"https://api.vk.com/method/users.get?v=5.71"
                            f"&access_token={ACCESS_TOKEN}&user_ids={uid}")
     friends_logger.debug("Sending %s request: '%s'",
@@ -43,28 +52,106 @@ def get_user_id_by_username_or_id(uid: str) -> Optional[int]:
     request.raise_for_status()
     response = request.json()
     try:
-        user_id = response.get('response')[0]['id']
+        user_id = response['response'][0]['id']
         friends_logger.debug("Got response: '%s'", str(response))
+        if not uid.isnumeric():
+            friends_logger.info("Got user_id %d for uid '%s'", user_id, uid)
         return user_id
-    except (IndexError, TypeError):
-        friends_logger.warning("Error getting user id for uid='%s'. "
-                               "Response: '%s'", uid, str(response))
+    except (KeyError, IndexError):
+        friends_logger.error("Error getting user id for uid='%s'. "
+                             "Response: '%s'", uid, str(response))
         return None
+
+
+def get_user_friends(
+        user_id: int, *fields: str
+) -> Optional[List[Dict[str, Any]]]:
+    """Return list of user's friends.
+
+    Gets user's friends with their attributes.
+    Returns list of friends or None if unable to get data for user_id.
+
+    :param user_id: User id.
+    :param fields: Additional attributes of friend, 'bdate' if not provided.
+
+    API Method docs: https://vk.com/dev/friends.get
+
+    VK API version: 5.71
+    """
+    if not fields:
+        fields = ('bdate',)
+
+    request = requests.get(f"https://api.vk.com/method/friends.get?v=5.71"
+                           f"&access_token={ACCESS_TOKEN}&user_id="
+                           f"{user_id}&fields={fields}")
+    friends_logger.debug("Sending %s request: '%s'",
+                         request.request.method, request.request.url)
+    request.raise_for_status()
+    response = request.json()
+    try:
+        friends_list = response['response']['items']
+        friends_logger.debug("Got response: '%s'", str(response))
+        friends_logger.info('Got %d friends total', len(friends_list))
+        return friends_list
+    except KeyError:
+        friends_logger.error("Error getting friends for user "
+                             "with user_id='%s'. Response: '%s'",
+                             user_id, str(response))
+        return None
+
+
+def get_friends_id_age_pairs(
+        friends_list: List[Dict[str, Any]]
+) -> List[Tuple[int, int]]:
+    """Return list of friends' ids and ages.
+
+    :param friends_list: List of friends' data dictionaries.
+    """
+    id_age_pairs = []
+    for friend in friends_list:
+        if 'bdate' in friend.keys():
+            age = _get_age_from_birthdate(friend['bdate'])
+            if age:
+                id_age_pairs.append((friend['id'], age))
+    return id_age_pairs
+
+
+def _get_age_from_birthdate(birthdate: str) -> Optional[int]:
+    """Return age for a given full birthdate or None if there is no year
+    in birthdate.
+
+    Ignores day and month of birth in calculation.
+    """
+    if not re.match(r'\d{1,2}\.\d{1,2}\.\d{4}$', birthdate):
+        return None
+    birthyear = int(birthdate[-4:])
+    age = date.today().year - birthyear
+    return age
 
 
 def calc_age(uid: str) -> List[Tuple[int, int]]:
     """Return list of (age, number_of_friends_of_this_age) pairs
-    ordered in descending order by second key first, then in ascending
-    order by first key.
-
+    ordered by second key descending first, then by first key ascending.
     """
     user_id = get_user_id_by_username_or_id(uid)
-    if user_id is None:
+    if not user_id:
         return []
-
-    return []
+    friends_list = get_user_friends(user_id)
+    if not friends_list:
+        return []
+    friends_list_with_ages = get_friends_id_age_pairs(friends_list)
+    friends_logger.info('Calculated ages for %d friends',
+                        len(friends_list_with_ages))
+    counted_ages = Counter([
+        age
+        for friend_id, age in sorted(friends_list_with_ages,
+                                     key=lambda x: x[1])
+    ])
+    return sorted(list(counted_ages.items()),
+                  key=lambda x: x[1], reverse=True)
 
 
 if __name__ == '__main__':
     res = calc_age('reigning')
+    friends_logger.info('Result: %s', res)
     print(res)
--- req/test_friends.py
+++ req/test_friends.py
@@ -1,18 +1,110 @@
 import unittest
-from req.friends import get_user_id_by_username_or_id, ACCESS_TOKEN
+from unittest.mock import patch, Mock
+
+from req.friends import (
+    get_user_id_by_username_or_id,
+    get_user_friends,
+    get_friends_id_age_pairs,
+    _get_age_from_birthdate,
+    calc_age,
+)
 
 
 class TestFriends(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.token = ACCESS_TOKEN
+        self.username = 'reigning'
+        self.user_id = 150617534
+        self.friends_list = [
+            {'id': 6477, 'first_name': 'Oksana', 'last_name': 'Ozernaya'},
+            {'id': 96163, 'first_name': 'Sergey', 'last_name': 'Kushlevich',
+             'bdate': '5.3.1986'},
+            {'id': 135863, 'first_name': 'Alexander', 'last_name': 'Gorny', },
+            {'id': 198261, 'first_name': 'Sveta', 'last_name': 'Snegireva',
+             'bdate': '27.4'},
+            {'id': 274123, 'first_name': 'Ilya', 'last_name': 'Penyaev',
+             'bdate': '20.7.1989'},
+            {'id': 5673757, 'first_name': 'Evgenia',
+             'last_name': 'Chernobrovkina', 'bdate': '5.5.1994'},
+            {'id': 5844771, 'first_name': 'Adelaida',
+             'last_name': 'Titengauzen', 'bdate': '13.3.1994'},
+        ]
+        self.friends_count = 7
 
     def test_get_user_id(self):
-        cases = ('reigning', '150617534', '', 'd')
-        expected_results = (150617534, 150617534, None, None)
-        for case, expected in zip(cases, expected_results):
+        cases = (
+            (self.username, self.user_id),
+            (str(self.user_id), self.user_id),
+            ('', None),
+            ('d', None),
+        )
+        for case, expected in cases:
             with self.subTest(case=case):
                 self.assertEqual(get_user_id_by_username_or_id(case), expected)
+
+    @patch('req.friends.requests.get')
+    def test_get_user_friends(self, mocked_get):
+        mocked_response = Mock()
+        mocked_get.return_value = mocked_response
+        mocked_response.request.method = 'GET'
+        mocked_response.request.url = (
+            f"https://api.vk.com/method/friends.get?v=5.71"
+            f"&access_token=test"
+            f"&user_id={self.user_id}&fields={'bdate'}"
+        )
+        mocked_response.json.return_value = {
+            'response': {
+                "count": self.friends_count,
+                "items": self.friends_list
+            }
+        }
+        self.assertEqual(get_user_friends(self.user_id), self.friends_list)
+
+    def test_get_user_friends_for_invalid_id(self):
+        self.assertEqual(get_user_friends(0), None)
+
+    def test_get_friends_id_age_pairs(self):
+        expected = [
+            (96163, 34),
+            (274123, 31),
+            (5673757, 26),
+            (5844771, 26),
+        ]
+        self.assertEqual(get_friends_id_age_pairs(self.friends_list), expected)
+
+    @patch('req.friends.get_user_friends')
+    @patch('req.friends.get_user_id_by_username_or_id')
+    def test_calc_age(self, mocked_get_id, mocked_get_friends):
+        mocked_get_id.return_value = self.user_id
+        mocked_get_friends.return_value = self.friends_list
+        expected = [
+            (26, 2),
+            (31, 1),
+            (34, 1),
+        ]
+        self.assertEqual(calc_age(self.username), expected)
+
+    def test_calc_age_with_invalid_uid(self):
+        self.assertEqual(calc_age(''), [])
+
+    def test_calc_age_for_user_with_no_friends(self):
+        self.assertEqual(calc_age('durov'), [])
+
+    def test_get_age_from_birthdate(self):
+        from datetime import date
+        current_year = date.today().year
+        cases = (
+            ('', None),
+            ('1994', None),
+            ('1.1', None),
+            ('12.12.12412', None),
+            ('12.12.1990', current_year - 1990),
+            ('11.11.111', None),
+            ('1.1.2000', current_year - 2000),
+        )
+        for case, expected in cases:
+            with self.subTest(case=case):
+                self.assertEqual(_get_age_from_birthdate(case), expected)
 
 
 if __name__ == '__main__':
