3ad52c851220a702d973f96ec6e5e051eaaeea39
Egor Yakubov <52618928+gorskii@users.noreply.github.com>
2021-02-04 20:45:14 +0300

    Implement echo view. Test template rendering.
    Test parameters passing with both GET and POST methods.
    Test X-Print-Statement header setting with both GET and POST methods.

--- template/tests/echo/test_echo.py
+++ template/tests/echo/test_echo.py
@@ -2,7 +2,7 @@ from django.test import SimpleTestCase
 
 
 class TestEchoView(SimpleTestCase):
-    """Test echo view"""
+    """Test echo view and template rendering"""
 
     def setUp(self):
         self.url = '/template/echo/'
@@ -16,11 +16,41 @@ class TestEchoView(SimpleTestCase):
         self.assertContains(response, 'statement is empty',
                             status_code=200, html=True)
 
-    def test_post_without_parameters(self):
-        pass
-
     def test_get_with_x_print_statement_header_set(self):
-        pass
+        response = self.client.get(self.url, HTTP_X_PRINT_STATEMENT='test')
+        self.assertContains(response, 'statement is test',
+                            status_code=200, html=True)
 
     def test_get_with_parameters(self):
-        pass
+        TESTCASES = (
+            ('?a=1', 'get a: 1 statement is empty'),
+            ('?c=2', 'get c: 2 statement is empty'),
+            ('?b=3&d=4', 'get b: 3 d: 4 statement is empty'),
+        )
+        for query, expected in TESTCASES:
+            with self.subTest(case=query, expected=expected):
+                response = self.client.get(self.url + query)
+                self.assertContains(response, expected,
+                                    status_code=200, html=True)
+
+    def test_post_without_parameters_returns_statement_is_empty(self):
+        response = self.client.post(self.url)
+        self.assertContains(response, 'statement is empty',
+                            status_code=200, html=True)
+
+    def test_post_with_x_print_statement_header_set(self):
+        response = self.client.post(self.url, HTTP_X_PRINT_STATEMENT='test')
+        self.assertContains(response, 'statement is test',
+                            status_code=200, html=True)
+
+    def test_post_with_parameters(self):
+        TESTCASES = (
+            ({'b': 1}, 'post b: 1 statement is empty'),
+            ({'d': 3}, 'post d: 3 statement is empty'),
+            ({'a': 2, 'b': 4}, 'post a: 2 b: 4 statement is empty'),
+        )
+        for data, expected in TESTCASES:
+            with self.subTest(case=data, expected=expected):
+                response = self.client.post(self.url, data=data)
+                self.assertContains(response, expected,
+                                    status_code=200, html=True)
--- template/views.py
+++ template/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def echo(request):
-    return render(request, 'echo.html')
+    if request.method == 'POST':
+        request_attrs = request.POST
+    elif request.method == 'GET':
+        request_attrs = request.GET
+    else:
+        request_attrs = {}
+
+    return render(request, 'echo.html', context={
+        'statement_header': request.META.get('HTTP_X_PRINT_STATEMENT'),
+        'a': request_attrs.get('a'),
+        'b': request_attrs.get('b'),
+        'c': request_attrs.get('c'),
+        'd': request_attrs.get('d'),
+    })
 
 
 def filters(request):
