9638df614a0fe5281bb85abb5b51c8cac29ba434
Egor Yakubov <52618928+gorskii@users.noreply.github.com>
2021-02-05 19:12:20 +0300

    Add inc filter and division template tag. Test template rendering.

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,22 @@
 from django import template
+
+register = template.Library()
+
+
+@register.filter
+def inc(number, increment):
+    """Increment number by given increment"""
+    return int(number) + int(increment)
+
+
+@register.simple_tag
+def division(first, second, to_int=False):
+    """Divide first number by second number
+
+    If to_int=True cast result to int.
+    """
+    try:
+        res = int(first) / int(second)
+    except ZeroDivisionError as e:
+        return f'Error: {e}'
+    return int(res) if to_int else res
